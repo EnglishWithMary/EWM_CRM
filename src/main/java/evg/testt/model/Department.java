6109cf33d4c133200b8ce79c4b75d880c1be58e1
StanislavX <sayusko86@gmail.com>
2016-10-20 22:26:10 +0300

    code refactoring (oval validation)

--- src/main/java/evg/testt/model/Department.java
+++ src/main/java/evg/testt/model/Department.java
@@ -8,14 +8,15 @@ import java.util.List;
 
 import net.sf.oval.constraint.Length;
 import net.sf.oval.constraint.MatchPattern;
+import net.sf.oval.constraint.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Entity(name = "departments")
 public class Department extends BaseModel{
 
 //    @Autowired
+//    @MatchPattern(pattern = "[A-Za-z0-9\\s]", message = "Department name should consist only of chars and digits")
     @Length(min = 3, max = 20, message = "The size of Department name should be between 3 and 20 included")
-    @MatchPattern(pattern = "[A-Za-z0-9\\s]", message = "Department name should consist only of chars and digits")
     private String name;
 
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
