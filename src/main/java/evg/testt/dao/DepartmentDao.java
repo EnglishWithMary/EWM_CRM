b53a75acb70dc2fb7fbe4d0ad9d77f089fdbd3ae
Balobanov <1vds@mail.ru>
2016-10-30 23:07:04 +0400

    Have added logging. Class evg.testt.logger.Logging intercepts invocations of all methods in all classes and writes name of class and method in log file. Have added <aop:aspectj-autoproxy/> tag to dispatcherServlet to activate AOP in Spring MVC.

--- src/main/java/evg/testt/dao/DepartmentDao.java
+++ src/main/java/evg/testt/dao/DepartmentDao.java
@@ -2,6 +2,8 @@ package evg.testt.dao;
 
 import evg.testt.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
 
 public interface DepartmentDao extends JpaRepository<Department, Integer> {
 
