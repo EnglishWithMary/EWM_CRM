fc3cd32537d792b1c79e28028c262fa9b811d840
Balobanov <1vds@mail.ru>
2016-11-03 16:00:02 +0400

    Added error page for spring security access errors and  errors 404. Added controller for errors. Added 2 images for errors.

---
+++ src/main/java/evg/testt/controller/ErrorController.java
@@ -0,0 +1,45 @@
+package evg.testt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+/**
+ * Created by User on 03.11.2016.
+ */
+@Controller
+public class ErrorController {
+    @RequestMapping(value = "/error403", method = RequestMethod.GET)
+    public ModelAndView accesssDenied() {
+
+        ModelAndView model = new ModelAndView();
+
+        //check if user is login
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetail = (UserDetails) auth.getPrincipal();
+            model.addObject("username", userDetail.getUsername());
+        }
+
+        model.setViewName("error");
+        return model;
+
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handle(NoHandlerFoundException ex) {
+
+        return "redirect:/error";
+    }
+}
+
