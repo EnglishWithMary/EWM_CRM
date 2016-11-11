040dc7fcf3ec44946a5207e324d4cd7fa81e8c6e
DENNNN <1vds@mail.ru>
2016-11-12 02:57:22 +0300

    404/500 errors via web.xml

--- src/main/java/evg/testt/controller/ErrorController.java
+++ src/main/java/evg/testt/controller/ErrorController.java
@@ -2,51 +2,43 @@ package evg.testt.controller;
 
 import evg.testt.util.JspPath;
 import org.apache.tiles.request.jsp.JspUtil;
+import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.multiaction.NoSuchRequestHandlingMethodException;
 
 /**
  * Created by User on 03.11.2016.
  */
-@Controller
+@ControllerAdvice
 public class ErrorController {
     @RequestMapping(value = "/error403", method = RequestMethod.GET)
     public ModelAndView accessDenied() {
-
-        ModelAndView model = new ModelAndView(JspPath.ERROR);
-
-        //check if user is login
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (!(auth instanceof AnonymousAuthenticationToken)) {
-            UserDetails userDetail = (UserDetails) auth.getPrincipal();
-            model.addObject("username", userDetail.getUsername());
-        }
-
-        model.setViewName("error");
-        return model;
-
+        return new ModelAndView(JspPath.ERROR);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handle(NoHandlerFoundException ex) {
-        return "redirect:/pageNotFound";
+    public ModelAndView pageNotFound(NoHandlerFoundException ex) {
+        return new ModelAndView(JspPath.ERROR);
     }
 
-    @RequestMapping("/pageNotFound")
-    public ModelAndView showErrorPage()
-    {
-       return new ModelAndView(JspPath.ERROR);
+    @ExceptionHandler(ConversionNotSupportedException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView iternalError(ConversionNotSupportedException ex) {
+        return new ModelAndView(JspPath.ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView anyException(Exception ex) {
+        return new ModelAndView(JspPath.ERROR);
     }
 }
 
