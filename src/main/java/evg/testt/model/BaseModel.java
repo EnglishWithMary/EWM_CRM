3fe60fc0929dab574adcff8618d9e1c5bbe31dd2
ionex <bessonov.evgeniy@gmail.com>
2016-11-12 01:22:23 +0200

    @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/evg/testt/model/BaseModel.java
+++ src/main/java/evg/testt/model/BaseModel.java
@@ -9,7 +9,7 @@ import lombok.Builder;
 public @Data class BaseModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Override
