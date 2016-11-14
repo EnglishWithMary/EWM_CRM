2d44fa1af3a8319c105409280c205c8776fca32c
ionex <bessonov.evgeniy@gmail.com>
2016-11-14 12:19:47 +0200

    test model links for save person

--- src/main/java/evg/testt/dao/StudentDao.java
+++
@@ -1,8 +0,0 @@
-package evg.testt.dao;
-
-import evg.testt.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentDao extends JpaRepository<Student, Integer> {
-
-}
--- src/main/java/evg/testt/service/AdminService.java
+++
@@ -1,6 +0,0 @@
-package evg.testt.service;
-
-import evg.testt.model.Admin;
-
-public interface AdminService extends Service<Admin> {
-}
