eb39fc06b3b89ad402dc3b1d350aec0e86a3a7a0
DENNNN <1vds@mail.ru>
2016-11-16 14:32:40 +0300

    Merge branch 'dev' into feature/leads-students-teachers-loginForm
    # Conflicts:
    #	src/main/java/evg/testt/controller/ManagerController.java
    #	src/main/java/evg/testt/controller/TeacherController.java
    #	src/main/java/evg/testt/model/Lead.java
    #	src/main/webapp/WEB-INF/pages/teachers/add.jsp
    #	src/main/webapp/WEB-INF/pages/teachers/all.jsp
    #	src/main/webapp/WEB-INF/tiles/teacher.xml

--- src/main/java/evg/testt/util/JspPath.java
+++ src/main/java/evg/testt/util/JspPath.java
@@ -12,8 +12,6 @@ public class JspPath {
 
     public static final String MANAGER_ALL = "managers/all";
     public static final String MANAGER_ADD = "managers/add";
-    public static final String TEACHER_ALL = "teachers/all";
-    public static final String TEACHER_ADD = "teachers/add";
     public static final String STUDENT_ALL = "students/all";
     public static final String STUDENT_ADD = "students/add";
 
