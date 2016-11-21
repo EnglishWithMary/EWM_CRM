39aea69716a3ac676abd96c9dea9cfe396a616cf
DENNNN <1vds@mail.ru>
2016-11-21 16:43:17 +0300

    Added pagination

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -9,6 +9,6 @@ INSERT INTO `ewm`.`persons` (`id`, `firstName`, `lastName`, `middleName`)
 VALUES (1, 'Ivan', 'Ivanov', 'Ivanovich');
 
 INSERT INTO `ewm`.`users` (`id`, `login`, `password`,`role_id`)
-VALUES (1, 'admin', '$2a$06$nHtGtWw0P93FjebcsovuQ.HUVrr8PmLwa1iwowokL3niZa7iXrjFm', 1); -- password-admin
+VALUES (1, 'admin', '$2a$08$4ozu21fedsIgpyrsIV1DOuGVVNkYyp6/KTLMil0gQdAz1r./qC3ri', 1); -- password-admin
 
 INSERT INTO `ewm`.`admins` (`id`, `person_id`, `user_id`) VALUES (1, 1, 1);
