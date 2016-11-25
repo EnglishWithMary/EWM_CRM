beafa9bae2ce5db91d4397f4a553c39b10a288e4
Bessonov.Evgeniy <bessonov.evgeniy@gmail.com>
2016-11-25 15:01:06 +0200

    Migrate to remote DB on PostgreSQL

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,18 +1,15 @@
-USE ewmdb;
+INSERT INTO roles (id, role) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO roles (id, role) VALUES (2, 'ROLE_MANAGER');
+INSERT INTO roles (id, role) VALUES (3, 'ROLE_TEACHER');
+INSERT INTO roles (id, role) VALUES (4, 'ROLE_STUDENT');
 
-INSERT INTO ewmdb.roles (id, role) VALUES (1, 'ROLE_ADMIN');
-INSERT INTO ewmdb.roles (id, role) VALUES (2, 'ROLE_MANAGER');
-INSERT INTO ewmdb.roles (id, role) VALUES (3, 'ROLE_TEACHER');
-INSERT INTO ewmdb.roles (id, role) VALUES (4, 'ROLE_STUDENT');
+INSERT INTO persons (id, firstname, lastname, middlename,registrationdate)
+VALUES (1, 'Ivan', 'Ivanov', 'Ivanovich', now());
 
-INSERT INTO ewmdb.persons (id, firstName, lastName, middleName,registrationDate)
-VALUES (1, 'Ivan', 'Ivanov', 'Ivanovich', NOW());
-
-INSERT INTO ewmdb.users (id, login, password,role_id)
+INSERT INTO users (id, login, password,role_id)
 VALUES (1, 'admin', '$2a$08$4ozu21fedsIgpyrsIV1DOuGVVNkYyp6/KTLMil0gQdAz1r./qC3ri', 1); -- password-admin
 
-INSERT INTO ewmdb.admins (id, person_id, user_id) VALUES (1, 1, 1);
+INSERT INTO admins (id, person_id, user_id) VALUES (1, 1, 1);
 
-
-INSERT INTO ewmdb.pipetypes (id, type) VALUES (1, 'LEAD_PIPE');
-INSERT INTO ewmdb.pipetypes (id, type) VALUES (2, 'STUDENT_PIPE');
+INSERT INTO pipetypes (id, type) VALUES (1, 'LEAD_PIPE');
+INSERT INTO pipetypes (id, type) VALUES (2, 'STUDENT_PIPE');
