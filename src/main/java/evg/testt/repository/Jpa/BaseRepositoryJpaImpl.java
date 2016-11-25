75890ea4696591ac394152417476929b10e51e00
Dmitriy Litvinenko <doknenivtil@gmail.com>
2016-11-25 12:55:28 +0200

    person sorting by registration date is refactored and merged with dev

--- src/main/java/evg/testt/repository/Jpa/BaseRepositoryJpaImpl.java
+++ src/main/java/evg/testt/repository/Jpa/BaseRepositoryJpaImpl.java
@@ -100,7 +100,6 @@ public abstract class BaseRepositoryJpaImpl<T extends BaseModel> implements Base
         }
         return hasPerson;
     }
-
     @Override
     public List<T> findSortedByRegistrationDate() throws SQLException {
         if(!hasPerson())throw new PersonFieldTypeNotFoundException(entityClass.getName() +
