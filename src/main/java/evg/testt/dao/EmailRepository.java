901f64dd1fa2b04a6aaa57509d259a7f195e9ff7
Dmitriy Litvinenko <doknenivtil@gmail.com>
2016-12-07 14:13:04 +0200

    modal window is added on deleating or trashing leads(refactored)

--- src/main/java/evg/testt/dao/EmailRepository.java
+++ src/main/java/evg/testt/dao/EmailRepository.java
@@ -1,10 +1,7 @@
 package evg.testt.dao;
 
 import evg.testt.model.Email;
-import evg.testt.model.Person;
 
-import java.sql.SQLException;
 
 public interface EmailRepository extends BaseRepository<Email> {
-    Email findByPerson(Person person) throws SQLException;
 }
