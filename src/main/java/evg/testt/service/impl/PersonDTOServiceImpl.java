d9c1997f97ca6678e2df68803fc7390dba34c663
Bessonov.Evgeniy <bessonov.evgeniy@gmail.com>
2017-01-19 18:28:00 +0200

    fix saving new Persons

--- src/main/java/evg/testt/service/impl/PersonDTOServiceImpl.java
+++ src/main/java/evg/testt/service/impl/PersonDTOServiceImpl.java
@@ -33,8 +33,8 @@ public class PersonDTOServiceImpl<T extends RegisteredUser> implements PersonDTO
 
     public Person getUpdatedPerson(Person person, PersonDTO personDTO) throws ParseException {
 
-        if(personDTO!= null) {
-            if (person == Person.NULL) {
+        if(personDTO != null) {
+            if (person == null) {
                 person = new Person();
             }
 
