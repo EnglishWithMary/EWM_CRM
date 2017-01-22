4add0cc4afb641c83adafe886c0ce667e00a1633
oleksiy musiyezdov <musiyezdov@gmail.com>
2017-01-22 15:34:38 +0200

    working...

--- src/main/java/evg/testt/dao/TeacherRepository.java
+++ src/main/java/evg/testt/dao/TeacherRepository.java
@@ -2,5 +2,10 @@ package evg.testt.dao;
 
 import evg.testt.model.Teacher;
 
+import java.util.List;
+
 public interface TeacherRepository extends RegisteredUserRepository<Teacher> {
+
+    List<Teacher> findTeacherByLevel(int teacherLevel);
+
 }
--- src/main/java/evg/testt/service/impl/TeacherServiceImpl.java
+++ src/main/java/evg/testt/service/impl/TeacherServiceImpl.java
@@ -1,12 +1,27 @@
 package evg.testt.service.impl;
 
+import evg.testt.dao.GroupRepository;
 import evg.testt.model.Teacher;
 import evg.testt.dao.TeacherRepository;
 import evg.testt.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Service
 @Transactional
 public class TeacherServiceImpl extends RegisteredUserServiceImpl<Teacher, TeacherRepository> implements TeacherService {
+
+    @Autowired
+    TeacherRepository teacherRepository;
+
+    @Override
+    public List<Teacher> getTeacherByLevel(int teacherLevel) throws SQLException {
+        return teacherRepository.findTeacherByLevel(teacherLevel);
+
+    }
+
 }
