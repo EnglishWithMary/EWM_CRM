949786a0bf8f62ab95ad8922190d5bfacc2cd32b
nikolay <nikolaykochura92@gmail.com>
2017-01-29 17:53:12 +0200

    Notes for Student

--- src/main/java/evg/testt/dao/Jpa/NoteRepositoryJpaImpl.java
+++ src/main/java/evg/testt/dao/Jpa/NoteRepositoryJpaImpl.java
@@ -5,17 +5,17 @@ import evg.testt.model.Note;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public class NoteRepositoryJpaImpl extends BaseRepositoryJpaImpl<Note> implements NoteRepository {
     @Override
     public void saveNote(Note note) {
-//        if(note.getId() == null) {
-//            em.persist(note);
-//        }
-//        else {
-//            em.merge(note);
-//        }
+    }
+
+    public List<Note> selectLastFiveNotes(){
+        query = em.createQuery("SELECT manager FROM managers manager WHERE user_id =:id");
     }
 }
 
--- src/main/java/evg/testt/dao/NoteRepository.java
+++ src/main/java/evg/testt/dao/NoteRepository.java
@@ -2,7 +2,12 @@ package evg.testt.dao;
 
 import evg.testt.model.Note;
 
+import java.util.List;
+
 
 public interface NoteRepository extends BaseRepository<Note> {
     void saveNote(Note note);
+
+     List<Note> selectLastFiveNotes();
+
 }
